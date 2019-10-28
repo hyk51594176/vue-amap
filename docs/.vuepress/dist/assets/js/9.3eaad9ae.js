ee2d547f1e7975c311c34791919fb2a363e149e4
zhouhongkai <hongkai.zhou@st-creditech.com>
2019-10-28 10:11:26 +0800

    docs: 文档build

---
+++ docs/.vuepress/dist/assets/js/9.3eaad9ae.js
@@ -0,0 +1,88 @@
+;(window.webpackJsonp = window.webpackJsonp || []).push([
+  [9],
+  {
+    307: function(e, t, n) {
+      'use strict'
+      n.r(t)
+      var a = {
+          data: function() {
+            return {
+              zoom: 6,
+              center: [116.3, 39.1],
+              markers: [
+                {
+                  position: [120.15, 30.28],
+                  label: { content: '杭州' },
+                  icon: { imageSize: [20, 40], size: [20, 40], image: '/car.png' }
+                },
+                {
+                  position: [116.15, 39.28],
+                  label: { content: '北京' },
+                  icon: { imageSize: [20, 40], size: [20, 40], image: '/car.png' }
+                }
+              ],
+              data: [{ name: '轨迹0', path: [[116.340417, 39.1], [120.15, 30.28]] }],
+              renderOptions: {
+                pathLineStyle: { strokeStyle: 'red', lineWidth: 6, dirArrowStyle: !0 }
+              }
+            }
+          },
+          methods: {
+            getOptions: function(e) {
+              return {
+                loop: !0,
+                speed: 1e6,
+                pathNavigatorStyle: {
+                  width: 20,
+                  height: 40,
+                  content: e.Render.Canvas.getImageContent('/car.png', onload, onerror),
+                  strokeStyle: null,
+                  fillStyle: null,
+                  pathLinePassedStyle: {
+                    lineWidth: 6,
+                    strokeStyle: '#f34135',
+                    dirArrowStyle: { stepSpace: 15, strokeStyle: '#fff' }
+                  }
+                }
+              }
+            }
+          }
+        },
+        r = n(19),
+        i = Object(r.a)(
+          a,
+          function() {
+            var e = this,
+              t = e.$createElement,
+              n = e._self._c || t
+            return n(
+              'el-amap',
+              { staticStyle: { height: '400px' }, attrs: { zoom: e.zoom, center: e.center } },
+              [
+                e._l(e.markers, function(t) {
+                  return n(
+                    'el-amap-mark',
+                    e._b({ key: t.position[0] + '_' + t.position[1] }, 'el-amap-mark', t, !1)
+                  )
+                }),
+                e._v(' '),
+                n(
+                  'el-amap-pathsimplifier',
+                  { attrs: { data: e.data, renderOptions: e.renderOptions } },
+                  [n('el-amap-pathnavigator', { attrs: { getOptions: e.getOptions } })],
+                  1
+                )
+              ],
+              2
+            )
+          },
+          [],
+          !1,
+          null,
+          null,
+          null
+        )
+      t.default = i.exports
+    }
+  }
+])
